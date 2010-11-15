b5ec7d04e325d76e4ad283173f2481519d209995
tox234 <tox234@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-15 10:07:45 +0000

    Fixed naming issues
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/fluidpibase/trunk@40205 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -3,7 +3,7 @@
 $extensionClassesPath = t3lib_extMgm::extPath('fluidpibase') . 'Classes/';
 return array(
 	// Extendable class to use fluid with pi based extensions
-	'tx_fluidpibase' => $extensionClassesPath . 'FluidPiBase.php',
+	'tx_fluidpibase' => $extensionClassesPath . 'class.tx_fluidpibase.php',
 );
 
 ?>
